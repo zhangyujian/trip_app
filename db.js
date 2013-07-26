63c58ca6085f92b9ffcecdcd487b364fd3f4c92a
zhangyujian <zhangyujian00@gmail.com>
2013-07-26 09:11:47 +0800

    second commit

---
+++ db.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var config = require('./config').config;
+var ObjectId = Schema.ObjectId;
+
+mongoose.connect(config.db);
+
+var ScenicSchema = new Schema({  
+    id         :    { type : Number,default:0 },
+    title      :    { type : String },
+    province   :    { type : String },
+    city       :    { type : String },
+    area       :    { type : String },
+    geography  :    { type : String },
+    weather    :    { type : String },
+    grade      :    { type : String },
+    price      :    { type : String },
+    buildtime  :    { type : String },
+    category   :    { type : String },
+    content    :    { type : String },
+    img        :    [String],
+    publish    :    { type: Boolean, default: false },
+    date       :    { type: Date, default: Date.now },
+    son        :    [{ son_name: String, son_content: String, son_img: [String] }], 
+});
+
+mongoose.model('Scenic', ScenicSchema);
