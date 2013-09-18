f288fd41a0c9d973d480dbc33009044032e76f3a
zhangyujian <zhangyujian00@gmail.com>
2013-09-18 10:51:00 +0800

    new

--- js/fun.js
+++ js/fun.js
@@ -1,12 +1,52 @@
-x$( "header" ).html("完美旅行2");
+x$( "header" ).html("完美旅行3");
+/*
 x$("#xhr").xhr('http://121.199.29.125:3004/list',{
 	method: 'get',
 	async: true,
-	dataType: "json",
 	callback: function(){
-		alert(this.responseText);
+		x$("#xhr").html(this.responseText);// this.responseText is string
+		//console.log(this.responseText);
 	},
 	error: function(){
 		alert('error');
 	}
 });
+*/
+x$(window).xhr('test.json',{
+	method: 'get',
+	async: true,
+	callback: function(){
+		var t = this.responseText;// this.responseText is string
+		var arr = eval('('+t+')');
+		for (var i = 0; i<arr.length; i++){
+			var html = '<li><a href="' + arr[i]._id + '">' + arr[i].title +'</a></li>';
+			var old_html = x$("#thelist").html();
+			x$("#thelist").html(old_html + html);
+		}
+	},
+	error: function(){
+		alert('error');
+	}
+});
+x$(window).load(function(e) {
+x$("#thelist li").each(function(){//由于是html写入的dom 无法x$！
+	var that = x$(this);
+	//alert("hi");
+	that.on('click',function(){
+		that.xhr('test2.json',{
+			method: 'get',
+			async: true,
+			callback: function(){
+				var t = this.responseText;// this.responseText is string
+				var obj = eval('('+t+')');
+				that.html(obj.title);
+			},
+			error: function(){
+				alert('error');
+			}
+		});
+		return false;
+	});
+});
+});
+
